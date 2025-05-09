0da2952a0d09536202092621c36abefbe899dc40
Stohodiuk Denys <stogodukdenis1@gmail.com>
2025-05-09 23:22:51 +0300

    Second commit lab4

--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'store/index.html')
+
+
+def view1(request):
+    return render(request, 'store/view1.html')
+
+
+def view2(request):
+    return render(request, 'store/view2.html')
+
+
+def view3(request):
+    return render(request, 'store/view3.html')
+
+
+def view4(request):
+    return render(request, 'store/view4.html')
+
+
+def view5(request):
+    return render(request, 'store/view5.html')
