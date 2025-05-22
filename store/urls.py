6b1ee2341a38b2fefc37c63bda9530c46099b41b
Stohodiuk Denys <stogodukdenis1@gmail.com>
2025-05-22 21:12:12 +0300

    Fourth commit lab6

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('Список відеокарт/', views.view1, name='view1'),
-    path(' Інформація про відеокарту/', views.view2, name='view2'),
-    path('Бренди відеокарт/', views.view3, name='view3'),
-    path('Корзина/', views.view4, name='view4'),
-    path('Оформлення замовлення/', views.view5, name='view5'),
+    path('List of video cards', views.view1, name='view1'),
+    path('Information about the video card', views.view2, name='view2'),
+    path('Video card brands', views.view3, name='view3'),
+    path('Cart', views.view4, name='view4'),
+    path('Checkout', views.view5, name='view5'),
+
 ]
